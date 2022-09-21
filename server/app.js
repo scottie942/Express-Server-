424f71e709817ede5130e9caa56fdce622dcd82d
Scottie <scottie@cccareers.org>
2022-09-21 13:43:15 -0700

    format

--- server/app.js
+++ server/app.js
@@ -1,12 +1,8 @@
-// import files and packages up here
 const topSpots = require("./data.json")
-
-// create your express server below
 const express = require("express");
 const morgan = require("morgan")
 const app = express();
 
-// add your routes and middleware below
 app.use(morgan("tiny"));
 
 app.get("/data", (req, res) => {
@@ -17,5 +13,4 @@ app.get("/", (req, res) => {
     res.send("")
 })
 
-// finally export the express application
 module.exports = app;
